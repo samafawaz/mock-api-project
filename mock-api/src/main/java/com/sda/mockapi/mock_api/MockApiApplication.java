f14c5fc98c704e7170b9d72fb23e23644e0c08c0
Sama <fawazsama@yahoo.com>
2024-12-21 22:56:24 +0200

    merged event and hotel main

--- mock-api/src/main/java/com/sda/mockapi/mock_api/MockApiApplication.java
+++ mock-api/src/main/java/com/sda/mockapi/mock_api/MockApiApplication.java
@@ -3,11 +3,10 @@ package com.sda.mockapi.mock_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  // This annotation enables component scanning
 public class MockApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MockApiApplication.class, args);
-	}
-
-} 
+    public static void main(String[] args) {
+        SpringApplication.run(MockApiApplication.class, args);
+    }
+}
